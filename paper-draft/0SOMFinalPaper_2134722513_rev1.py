7fa9d5e8fd693e165b5d3e1d6fed185c4c14ffb5
Vegard Kvernelv <vegard@nr.no>
2026-02-26 11:30:18 +0100

    Add dendrogram and large cluster plot

--- paper-draft/0SOMFinalPaper_2134722513_rev1.py
+++ paper-draft/0SOMFinalPaper_2134722513_rev1.py
@@ -385,8 +385,10 @@ print()
 if number_of_clusters is None:
     print(f"Number of clusters picked based on Silhouette     : {selected_cluster['Number of clusters']}")
     # print(f"Number of clusters picked based on DBI         : {selected_cluster['Number of clusters']}")
+    final_n_clusters = selected_cluster['Number of clusters']
 else:
     print(f"Number of clusters fixed to                    : {number_of_clusters}")
+    final_n_clusters = number_of_clusters
 
 # fig = px.line(cluster_df, x='Number of clusters', y='DBI', markers=True)
 # fig.layout.title = "Davies-Bouldin Index (lower is better)"
@@ -548,22 +550,22 @@ def add_som_features(fig: go.Figure, row, col, feature_index, colorbar_settings)
     )
 
 
-def add_training_cells(fig: go.Figure, row, col):
+def add_training_cells(fig: go.Figure, row, col, show_sum = True):
     # Add colored hexagons
     label_colors = DEFAULT_PLOTLY_COLORS
     for i in range(final_nx):
         for j in range(final_ny):
             text = ""
             if len(win_map[(i, j)]) > 0:
-                samples = [str(s) for s in sorted(df.iloc[win_map[(i, j)]].index.values.tolist())]
-                if len(samples) <= 3:
-                    text = f"{','.join(samples)}"
-                elif len(samples) <= 6:
-                    text = f"{','.join(samples[:3])}" + f"<br>{','.join(samples[3:])}"
-                elif len(samples) <= 9:
-                    text = f"{','.join(samples[:3])}" + f"<br>{','.join(samples[3:6])}" + f"<br>{','.join(samples[6:])}"
+                if show_sum:
+                    text = f"{len(win_map[(i, j)])}"
                 else:
-                    text = f"{','.join(samples[:3])}" + f"<br>{','.join(samples[3:-3])}" + f"<br>{','.join(samples[-3:])}"
+                    if "Sample" in df.columns:
+                        names = df.iloc[win_map[(i, j)]]["Sample"]
+                    else:
+                        names = df.iloc[win_map[(i, j)]].index
+                    samples = [str(s) for s in sorted(names.values.tolist())]
+                    text = f"{'<br>'.join(samples)}"
 
             fig.add_scatter(
                 x=hexagons[i, j, :, 0],
@@ -639,6 +641,19 @@ for i, (row_index, col_index) in enumerate(subplot_ix):
     sp.yaxis.scaleanchor = sp.yaxis.anchor
     sp.yaxis.showticklabels = False
     sp.xaxis.showticklabels = False
+
+## -------------------------------------------------------
+# Large cluster plot
+## -------------------------------------------------------
+
+large_cluster_fig = go.Figure()
+add_training_cells(large_cluster_fig, None, None, show_sum=False)
+add_cluster_boundaries(large_cluster_fig, None, None)
+large_cluster_fig.layout.yaxis.scaleanchor = "x"
+large_cluster_fig.layout.yaxis.showticklabels = False
+large_cluster_fig.layout.xaxis.showticklabels = False
+
+show_figure(large_cluster_fig)
 
 ## -------------------------------------------------------
 # Draw isolines for SOM feature planes
@@ -2238,3 +2253,108 @@ for i, feature in enumerate(input_columns):
     print(f"{feature:<12} {loadings_biplot[i, 0]:>12.4f} {loadings_biplot[i, 1]:>12.4f} {vector_length:>14.4f}")
 
 print("="*50 + "\n")
+
+
+# -----------------------------------------
+# Dendrogram plot
+# -----------------------------------------
+fig = plt.figure(figsize=(10, 8))
+
+# Axis settings
+left, bottom, width, height = 0.1, 0.35, 0.8, 0.35
+ax1 = fig.add_axes([left, bottom, width, height])
+
+# Customize the colors of the clusters
+# colors=['#BA2F29', '#E9C832', '#8EBA42', '#67ACE6']
+# NB! First plotly color is skipped to align with the coloring above
+colors = [  # Convert from "rgb(123, 123, 123)" to "#445566"
+    '#%02X%02X%02X' % tuple(map(int, c.strip("rgb() ").split(",")))
+    for c in DEFAULT_PLOTLY_COLORS[1:]
+]
+hierarchy.set_link_color_palette(colors)
+
+# Plot the dendrogram using the built-in scipy function
+# Ideally, we could use maxclust directly, like we do with fcluster:
+#   hierarchy.fcluster(links, i, 'maxclust')
+# However, this is not supported in dendrogram, so we need to find
+# the appropriate distance threshold to get the desired number of clusters
+max_d = 0.7
+for _ in range(20):
+    # Don't plot yet, this is just to get the number of clusters
+    dendrogram = hierarchy.dendrogram(
+        links,
+        leaf_rotation=90,
+        leaf_font_size=0,
+        color_threshold=max_d,
+        above_threshold_color='grey',
+        no_plot=True
+    )
+    d_clusts = len(set(dendrogram['leaves_color_list']))
+    if d_clusts == final_n_clusters:
+        break
+    if d_clusts < final_n_clusters:
+        max_d *= 0.8
+    if d_clusts > final_n_clusters:
+        max_d *= 1.2
+else:
+    # This can happen if e.g. two clusters are very close together
+    print(f"Warning: Could not find a distance threshold to get {final_n_clusters} clusters")
+
+
+# Main dendrogram plot
+print(f"Using distance threshold {max_d:.3f} to get {d_clusts} clusters")
+dendrogram = hierarchy.dendrogram(
+        links,
+        leaf_rotation=90,
+        leaf_font_size=0,
+        color_threshold=max_d,
+        above_threshold_color='grey',
+)
+hierarchy.set_link_color_palette(None)
+ax1.axhline(y=max_d, linestyle='-.', color='k', lw=1.25) 
+ax1.set_ylabel('Linkage distance', fontsize=13)
+ax1.set_yticklabels([0, 5, 10, 15, 20, 25, 30, 35], fontsize=10)
+ax1.set_xticks([])
+ax1.spines['top'].set_linewidth(1.25)
+ax1.spines['bottom'].set_linewidth(1.25)
+ax1.spines['left'].set_linewidth(1.25)
+ax1.spines['right'].set_linewidth(1.25)
+# ax1.text(150, 20.5, '$Phenon$' + ' ' + '$Line$', fontsize=12)
+
+
+# The bottom figure showing the cluster names
+left, bottom, width, height = 0.1, 0.15, 0.8, 0.2
+ax2 = fig.add_axes([left, bottom, width, height])
+ax2.set_xlim(0, (len(links) + 1) * 10)
+ax2.set_ylim(0, 2)
+
+
+# Dot line to split each cluster 
+unique, counts = np.unique(dendrogram['leaves_color_list'], return_counts=True)
+cluster_size_dict = dict(zip(unique, counts))
+cluster_size = [
+    cluster_size_dict[clr]
+    for clr in dict.fromkeys(dendrogram['leaves_color_list'])
+]
+boundaries = np.cumsum(cluster_size) * 10
+for i in range(final_n_clusters - 1):
+    ax2.plot([boundaries[i], boundaries[i]], [1.5, 2.0], linestyle='--', color='k', lw=1.5) 
+
+ax2.set_xticks([])
+ax2.set_yticks([])
+    
+ax2.spines['top'].set_color('none')
+ax2.spines['bottom'].set_color('none')
+ax2.spines['left'].set_color('none')
+ax2.spines['right'].set_color('none')
+
+# Add cluster labels for arbitrary number of clusters
+extended_boundaries = np.concatenate([[0], boundaries])
+for i in range(final_n_clusters):
+    left_bound = extended_boundaries[i]
+    right_bound = extended_boundaries[i + 1]
+    center_x = left_bound + (right_bound - left_bound) / 2
+    ax2.text(center_x, 1.5, f'C{i+1} \nN={cluster_size[i]}', ha='center', va='bottom', fontsize=12)
+
+plt.show()
+# ----------------------------------------
